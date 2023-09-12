1c8e64e016a4c3f87be8b43cc7123975584d4f0a
manivanv <144762067+manivanv@users.noreply.github.com>
2023-09-12 06:41:17 +0000

    ec2 instance creation

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,13 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-02bb7d8191b50f4bb"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    key_name = "Terraform-key"
+    subnet_id = "subnet-0bc534fcf0d9e430f"
+    tags = {
+        Name = "tf-example"
+    }
 }
